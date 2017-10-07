01f8865781ff2e623e9ebd37049c9f081c06b582
Strudy <aravnarula18@gmail.com>
2017-10-07 16:50:43 -0400

    Create Sprict.js

---
+++ Sprict.js
@@ -0,0 +1,8 @@
+function makeSequence() {
+    var x = 20; // so it wraps to 0 first time
+    return function() {
+        if (x == 20) { x = 0 }
+        else { x += 4 }
+        return x;
+    }
+}
